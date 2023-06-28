cf4838979e4631916a5bf8ae872cc29776eb56b9
Pablo da Cunha <pablodacun@gmail.com>
2023-06-28 14:33:46 +0200

    h

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+document.getElementById("homeButton").onclick = function () {
+    window.location.hash = "index.html";
+    };
+
+document.getElementById("portfolioButton").onclick = function () {
+    window.location.replace("portfolio.html");
+    };
+
+document.getElementById("contactButton").onclick = function () {
+    window.location.hash = "contact.html";
+    };   

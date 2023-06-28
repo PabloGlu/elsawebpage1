c8dcb0d1e77a6588adc083f2ee11622a078d872e
Pablo da Cunha <pablodacun@gmail.com>
2023-06-28 12:36:06 +0200

    first commit

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+document.getElementById("homeButton").onclick = function () {
+    location.href = "index.html";
+    };
+
+document.getElementById("portfolioButton").onclick = function () {
+    location.href = "portfolio.html";
+    };
+
+document.getElementById("contactButton").onclick = function () {
+    location.href = "contact.html";
+    };   
